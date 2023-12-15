f22f5b2f847fd934b16a104af7eb0366028cc3a4
jiangxiaozhuang <jxzcomleader@qq.com>
2023-12-15 17:01:37 +0800

    修改代码

--- dolphinscheduler-worker/src/main/java/org/apache/dolphinscheduler/server/worker/utils/YarnAPi.java
+++ dolphinscheduler-worker/src/main/java/org/apache/dolphinscheduler/server/worker/utils/YarnAPi.java
@@ -1,6 +1,8 @@
 package org.apache.dolphinscheduler.server.worker.utils;
 
 
+import cn.hutool.core.util.StrUtil;
+import cn.hutool.http.HttpUtil;
 import org.apache.dolphinscheduler.plugin.task.api.enums.TaskExecutionStatus;
 import org.apache.dolphinscheduler.remote.utils.Host;
 import org.apache.dolphinscheduler.service.storage.impl.HadoopUtils;
@@ -9,11 +11,7 @@ import cn.hutool.http.HttpRequest;
 import cn.hutool.http.HttpResponse;
 import cn.hutool.json.JSONObject;
 import cn.hutool.json.JSONUtil;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Bean;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
 import java.util.Map;
 import java.util.List;
 
@@ -25,10 +23,11 @@ public class YarnAPi {
      * @param logger 日志
      */
 
-    /*@Value("${yarnHostName}")
-    private static String yarnHostName;*/
     public static void cancelApplication(Host host, List<String> appIds, Logger logger) {
         String yarnHostName = System.getenv("yarnHostName");
+        String principal = System.getenv("principalName");
+        String serviceCluster = System.getenv("serviceCluster");
+        String cookie = System.getenv("cookie"); //JSESSIONID=1xvqyk3z2z3qi1slj01ehf7l6w
 
         if (appIds == null || appIds.isEmpty()) {
             return;
@@ -37,48 +36,87 @@ public class YarnAPi {
         for (String appId : appIds) {
             try {
                 TaskExecutionStatus applicationStatus = HadoopUtils.getInstance().getApplicationStatus(appId);
-
                 if (!applicationStatus.isFinished()) {
-//                    String filePath = "/opt/soft/resourcemanager.txt";
-//                    BufferedReader reader = new BufferedReader(new FileReader(filePath));
-//                    String line;
-//                    while ((line = reader.readLine()) != null) {
-//                        // 处理每一行的值
-//                        logger.info("=====================line：====" + line + "==========================");
-//                    }
-                    logger.info("=====================yarnHostName：====" + yarnHostName + "==========================");
-                    String url = "http://" + yarnHostName + ":8088/ws/v1/cluster/apps/" + appId + "/state?user.name=hdfs";
-                    logger.info("======================url:====" + url + "==========================");
-                    //查询状态
-                    HttpRequest request = HttpRequest.get(url);
-                    // 发送请求并获取响应结果
-                    HttpResponse response = request.execute();
-                    // 将响应结果转换为JSON字符串
-                    String jsonStr = response.body();
-                    // 将JSON字符串转换为Map对象，方便后续处理
-                    Map<String, Object> resultMap = JSONUtil.parseObj(jsonStr);
-                    // 输出结果
-                    String status = resultMap.get("state").toString();
-                    logger.info("=========================目前任务状态为：" + status);
-                    if (!status.equals("FAILED") && !status.equals("KILLED")) {
-                        HttpRequest httpRequest = HttpRequest.put(url);
-                        // 发送请求并获取响应结果
-                        JSONObject jsonObject = new JSONObject();
-                        jsonObject.put("state", "KILLED");
-                        String string = jsonObject.toString();
-                        httpRequest.body(string);
-                        HttpResponse res = httpRequest.execute();
-                        // 将响应结果转换为JSON字符串
-                        String str = res.body();
-                        // 将JSON字符串转换为Map对象，方便后续处理
-                        Map<String, Object> map = JSONUtil.parseObj(str);
-                        // 输出结果
-                        logger.info("=========================调用Yarn Api修改任务状态为：" + map.toString());
+                    logger.info("=====================yarnHostName：" + yarnHostName + "==========================");
+                    String url = yarnHostName + appId + "/state?user.name=" + principal;
+                    logger.info("======================url：" + url + "==========================");
+                    //开关 区分华为FI 与ambari
+                    switch (serviceCluster) {
+                        case "huawei":
+                            handleHuaweiServiceCluster(url, logger, cookie);
+                            break;
+                        case "ambari":
+                            handleAmbariServiceCluster(url, logger);
+                            break;
+                        default:
+                            logger.error("获取不到大数据平台名称");
+                            throw new NullException("获取不到大数据平台名称");
                     }
                 }
             } catch (Exception e) {
-                logger.error("Get yarn application app id [{}}] status failed", appId, e);
+                logger.error("Get yarn application app id [{}] status failed", appId, e);
             }
+        }
+    }
+
+    private static void handleHuaweiServiceCluster(String url, Logger logger, String cookie) {
+        String result = HttpUtil
+                .createGet(url)
+                .header("Cookie", cookie)
+                .execute()
+                .charset("utf-8")
+                .body();
+        logger.info("=====================result：" + result);
+        if (StrUtil.isNotBlank(result)) {
+            Map<String, Object> resultMap = JSONUtil.parseObj(result);
+            String status = resultMap.get("state").toString();
+            logger.info("=====================任务状态为：" + status);
+            if (!status.equals("FAILED") && !status.equals("KILLED")) {
+                JSONObject jsonObject = new JSONObject();
+                jsonObject.put("state", "KILLED");
+                String string = jsonObject.toString();
+                String update = HttpRequest.put(url)
+                        .header("Cookie", cookie)
+                        .body(string)
+                        .execute()
+                        .body();
+                Map<String, Object> map = JSONUtil.parseObj(update);
+                String str = map.get("state").toString();
+                if (StrUtil.isNotBlank(str)) {
+                    logger.info("=====================任务状态修改成功=====================");
+                }
+            }
+        } else {
+            throw new NullException("请检查参数是否正确或Cookie是否存在");
+        }
+    }
+
+    private static void handleAmbariServiceCluster(String url, Logger logger) {
+        //查询状态
+        HttpRequest request = HttpRequest.get(url);
+        // 发送请求并获取响应结果
+        HttpResponse response = request.execute();
+        // 将响应结果转换为JSON字符串
+        String jsonStr = response.body();
+        // 将JSON字符串转换为Map对象，方便后续处理
+        Map<String, Object> resultMap = JSONUtil.parseObj(jsonStr);
+        // 输出结果
+        String status = resultMap.get("state").toString();
+        logger.info("=========================目前任务状态为：" + status);
+        if (!status.equals("FAILED") && !status.equals("KILLED")) {
+            HttpRequest httpRequest = HttpRequest.put(url);
+            // 发送请求并获取响应结果
+            JSONObject jsonObject = new JSONObject();
+            jsonObject.put("state", "KILLED");
+            String string = jsonObject.toString();
+            httpRequest.body(string);
+            HttpResponse res = httpRequest.execute();
+            // 将响应结果转换为JSON字符串
+            String str = res.body();
+            // 将JSON字符串转换为Map对象，方便后续处理
+            Map<String, Object> map = JSONUtil.parseObj(str);
+            // 输出结果
+            logger.info("=========================调用Yarn Api修改任务状态为：" + map);
         }
     }
 
